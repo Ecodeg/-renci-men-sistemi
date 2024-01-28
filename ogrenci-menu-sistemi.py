4268a9bba60b4abfc4799302b37ba78edde3ee7e
Ebrar Gümüş <131487971+Ecodeg@users.noreply.github.com>
2024-01-28 21:55:38 +0300

    Update ogrenci-menu-sistemi.py

--- ogrenci-menu-sistemi.py
+++ ogrenci-menu-sistemi.py
@@ -1,10 +1,3 @@
-"""
-•Öğrenci Menü Sistemi:
-~Öğrenci ekle, öğrenci sil, öğrenci bilgilerini göster, öğrenci sınav sonuçlarına eriş; Menü seçeneklerimiz olacaktır.
-~Fonksiyon bilgisi olanlar fonksiyonlar ile, olmayanlar ise if-elif ile yapacaktır.
-~Öğrenci adı, öğrenci soyadı, öğrenci numarası, 1. sınav sonucu ve 2. sınav sonucu bilgileri kullanıcıdan alınacaktır.
-
-"""
 ogrenci={}
 
 while True:
